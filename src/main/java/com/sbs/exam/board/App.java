713c1808b3ed972114925c44f56d58dd63a99909
Jeoneungi <jeg3339@naver.com>
2023-05-27 15:14:37 +0900

    JDBC SELECT 테스트

--- src/main/java/com/sbs/exam/board/App.java
+++ src/main/java/com/sbs/exam/board/App.java
@@ -2,11 +2,17 @@ package com.sbs.exam.board;
 
 import com.sbs.exam.board.container.Container;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
+  private Article lastArticle;
+
   public void run() {
     Scanner sc = Container.scanner;
     List<Article> articles = new ArrayList<>();
@@ -22,21 +28,74 @@ public class App {
         String title = sc.nextLine();
         System.out.printf("내용) ");
         String content = sc.nextLine();
-
         int id = ++articleLastId;
 
+        Connection conn = null;
+        PreparedStatement pstat = null;
+
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "root", "");
+
+          String sql = "INSERT INTO article";
+          sql += " SET regDate = NOW()";
+          sql += ", updateDate = NOW()";
+          sql += ", title = \"" + title + "\"";
+          sql += ", content = \"" + content + "\"";
+
+          pstat = conn.prepareStatement(sql);
+          int affectRows = pstat.executeUpdate();
+
+          System.out.println("affectRows : " + affectRows);
+          System.out.println("sql : " + sql);
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+        } catch (SQLException e) {
+          System.out.println("에러: " + e);
+        } finally {
+          try {
+            if (conn != null && !conn.isClosed()) {
+              conn.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (pstat != null && !pstat.isClosed()) {
+              pstat.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
         Article article = new Article(id, title, content);
+        lastArticle = article;
+
         System.out.println("생선된 게시물 객체 : " + article);
 
         articles.add(article);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
-
       }
-
       else if(cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
+
+        if(articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("번호 / 제목");
+
+        for(Article article : articles) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
       }
+
       else if(cmd.equals("system exit")) {
         System.out.println("프로그램 종료");
         break;
--- src/main/java/com/sbs/exam/board/Article.java
+++ src/main/java/com/sbs/exam/board/Article.java
@@ -2,11 +2,24 @@ package com.sbs.exam.board;
 
 public class Article {
   int id;
+  String regDate;
+  String updateDate;
   String title;
   String content;
 
+
   public Article(int id, String title, String content) {
     this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.title = title;
+    this.content = content;
+  }
+
+  public Article(int id, String regDate, String updateDate, String title, String content) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
     this.title = title;
     this.content = content;
   }
@@ -15,8 +28,12 @@ public class Article {
   public String toString() {
     return "Article{" +
         "id=" + id +
+        ", regDate='" + regDate + '\'' +
+        ", updateDate='" + updateDate + '\'' +
         ", title='" + title + '\'' +
         ", content='" + content + '\'' +
         '}';
   }
 }
+
+
